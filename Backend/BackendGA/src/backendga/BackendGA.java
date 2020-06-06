6b264b272d907b347971905947adf22cfeabaf97
LuisFelipeP98 <61294912+LuisFelipeP98@users.noreply.github.com>
2020-06-05 22:15:35 -0600

    Update BackendGA.java

--- Backend/BackendGA/src/backendga/BackendGA.java
+++ Backend/BackendGA/src/backendga/BackendGA.java
@@ -1,11 +1,29 @@
 package backendga;
 
 import backendga.modelo.dao.GestorCurso;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BackendGA {
 
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        
+        for(Integer i:list)
+            System.err.println(i);
+        
+        list.remove(2);
+        
+        for(Integer i:list)
+            System.err.println(i);
+        
     }
 
 }
